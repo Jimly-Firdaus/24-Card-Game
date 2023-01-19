2256674a0f8f3f5ea9f37c573eb40ea9866bd00d
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-01-19 19:22:39 +0700

    feat: permutations for choosen numbers

--- src/main.java
+++ src/main.java
@@ -22,8 +22,32 @@ public class main {
         for (String number : numberChoiceCollection) {
             System.out.print(number + " ");
         }
+
+        // Map to new dynamic ArrayList to append all permutation of the 4 numbers
+        ArrayList<String> numberChoiceArr = new ArrayList<>(Arrays.asList(numberChoiceCollection));
+        ArrayList<ArrayList<String>> permutationArr = new ArrayList<>();
+        permutations(numberChoiceArr, new ArrayList<String>(), permutationArr);
     }
 
+    /**
+     * Give all the permutation of the given array
+     * @param choosenNumArr given raw array
+     * @param perm container for current permutation
+     * @param permutationArr array containing all permutations
+     * */
+    public static void permutations(ArrayList<String> choosenNumArr, ArrayList<String> perm, ArrayList<ArrayList<String>> permutationArr) {
+        if (choosenNumArr.size() == 0) {
+            permutationArr.add(perm);
+        } else {
+            for (int i = 0; i < choosenNumArr.size(); i++) {
+                ArrayList<String> newItems = new ArrayList<>(choosenNumArr);
+                String newItem = newItems.remove(i);
+                ArrayList<String> newPerm = new ArrayList<>(perm);
+                newPerm.add(newItem);
+                permutations(newItems, newPerm, permutationArr);
+            }
+        }
+    }
 
     private static void printArr (String[] arr) {
         for (int i = 0; i < arr.length; i++){
